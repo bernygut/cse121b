79d9680045021ffda9e9883be254e6c197bfd8c0
bernygut <bernygutierrez@gmail.com>
2024-03-23 21:12:18 -0600

    Advance up to multipy. Run to test

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -3,22 +3,42 @@
 /* FUNCTIONS */
 /* Function Definition - Add Numbers */
 
-document.querySelector("#addNumbers").addEventListener("click", addNumbers);
-
 function add (number1, number2){
     return number1 + number2;
 }
 
 function addNumbers (){
-    let InputNumber1 = Number(document.querySelector("#add1").value);
-    let InputNumber2 = Number(document.querySelector("#add2").value);
-    document.querySelector("#sum").value = add(InputNumber1, InputNumber2);
+    let AddInputNumber1 = Number(document.querySelector("#add1").value);
+    let AddInputNumber2 = Number(document.querySelector("#add2").value);
+    document.querySelector("#sum").value = add(AddInputNumber1, AddInputNumber2);
 }
 
-/* Function Expression - Subtract Numbers */
+document.querySelector("#addNumbers").addEventListener("click", addNumbers);
 
+
+/* Function Expression - Subtract Numbers */
+function subtract (number1, number2){
+    return number1 - number2;
+}
+
+function subtractNumbers (){
+    let SubInputNumber1 = Number(document.querySelector("#subtract1").value);
+    let SubInputNumber2 = Number(document.querySelector("#subtract2").value);
+    document.querySelector("#sum").value = add(SubInputNumber1, SubInputNumber2);
+}
+
+document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
+
+function multiplyNumbers (){
+    let factorInputNumber1 = Number(document.querySelector("#factor1").value);
+    let factorInputNumber2 = Number(document.querySelector("#factor2").value);
+    document.querySelector("#product").value = (factorInputNumber1, factorInputNumber2) => factorInputNumber1 * factorInputNumber2;
+}
+
+document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
+
 
 
 /* Open Function Use - Divide Numbers */
